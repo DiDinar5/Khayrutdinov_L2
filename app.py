0cf4b663f500a085c329837ae120d7e8ab557d96
DiDinar5 <dinar-wp9@mail.ru>
2024-04-21 23:42:14 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    from team.sqlitebp import bp as sqlitebp
+    from team.mariabp import bp as mariabp
+
+    app = Flask(__name__)
+    bps = [
+        ["SQLite", sqlitebp],
+        ["MariaDB", mariabp],
+    ]
+    bps = sorted(bps)
+    for i, (title, bp) in enumerate(bps, start=1):
+        app.register_blueprint(bp, url_prefix=f"/{i}")
+
+    @app.route("/")
+    def index():
+        return render_template("index.tpl", bps=bps)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
